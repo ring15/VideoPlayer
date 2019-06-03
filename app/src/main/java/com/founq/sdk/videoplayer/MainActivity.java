43abb229bca60710b6e79700b510113f03abf159
孙瑞环 <sunruihuan@founq.com>
2019-06-03 10:56:48 +0800

    android原生视频播放器之利用Intent调用系统自带的播放器或者安装的第三方播放器

--- app/src/main/java/com/founq/sdk/videoplayer/MainActivity.java
+++ app/src/main/java/com/founq/sdk/videoplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.founq.sdk.videoplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_video:
+                //直接调用系统中的视频播放器
+                Uri uri = Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(uri, "video/*");
+                startActivity(intent);
+                break;
+        }
     }
 }
